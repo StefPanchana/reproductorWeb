0e51ac832123365df9c3448f1e56fbf0c9a418e7
DeniseBergmann <denichabepa@gmail.com>
2024-02-07 22:33:41 -0500

    Arreglo en las canciones

--- javascript.js
+++ javascript.js
@@ -23,19 +23,19 @@ let listSongs = [
     new Song("Aurora", "Dero", "00:04:00", "Oid Mortales Records", 1992, "Dance/Electronic", "2.jpeg"),
     new Song("Blah Blah Blah","Armin van Buuren","00:03:50","Live at Ultra Music Festival Miami 2018 (Highlights)",2018,"Dance/Electronic","3.jpg"),
     new Song("Fire on fire", "Sam Smith", "00:04:00", "Let’s Dance - Das Tanzalbum 2019", 2019,  "Dance/Electronic", "4.jpg"),
-    new Song("Marisola Remix", "Cris MJ, Standly, Nicki Nicole, Duki", 2022, "Urbano Latino", "Single"),
-    new Song("La Original", "Emilia, Tini", 2023, "Urbano Latino", ".mp3"),
-    new Song("Los del Espacio", "LIT killah, Tiago PZK, Maria Becerra, Duki, Emilia, Rusherking, Big One, FMK", 2023, "Urbano Latino", "Los Del Espacio"),
-    new Song("Bellakeo", "Peso Pluma, Anitta", 2023, "Urbano Latino", "Bellakeo"),
-    new Song("Perro Negro", "Bad Bunny ft. Feid", 2023, "Urbano Latino", "Nadi sabe lo que va a pasar mañana"),
-    new Song("Feliz Cumpleaños Ferxxo", "Feid", 2022, "Urbano Latino", "Feliz Cumpleaños Ferxxo"),
-    new Song("Provenza", "Karol G", 2022, "Urbano Latino", "Mañana sera bonito"),
-    new Song("Eternity", "Anyma & Chris Avantgarde", 2023, "Techno melódico", "Genesys"),
-    new Song("Syren", "Anyma & Rebūke", 2023, "Techno melódico", "Genesys"),
-    new Song("Simulation", "Anyma & Chris Avantgarde", 2023, "Techno melódico", "Genesys"),
-    new Song("Unearth", "Anyma", 2023, "Techno melódico", "Genesys"),
-    new Song("Aria", "Argy & Omnya", 2023, "Techno melódico", "Single"),
-    new Song("Real Love", "Martin Garrix & Lloyiso", 2023, "EDM", "Real Love")
+    new Song("Marisola", "Cris MJ, Standly, Nicki Nicole, Duki", "00:03:04", "Single", 2022, "Urbano Latino","5.jpeg"),
+    new Song("La Original", "Emilia, Tini", "00:02:21", ".mp3", 2023, "Urbano Latino", "6.jpeg"),
+    new Song("Los del Espacio", "LIT killah, Tiago PZK, Maria Becerra, Duki, Emilia, Rusherking, Big One, FMK", "00:05:38",  "Los Del Espacio", 2023, "Urbano Latino", "7.jpeg"),
+    new Song("Bellakeo", "Peso Pluma, Anitta", "00:03:17", "Bellakeo", 2023, "Urbano Latino", "8.jpeg"),
+    new Song("Perro Negro", "Bad Bunny ft. Feid", "00:02:43", "Nadi sabe lo que va a pasar mañana", 2023, "Urbano Latino", "9.jpeg"),
+    new Song("Feliz Cumpleaños Ferxxo", "Feid",  "00:02:36", "Feliz Cumpleaños Ferxxo", 2022, "Urbano Latino", "10.jpeg"),
+    new Song("Provenza", "Karol G", "00:03:30", "Mañana sera bonito", 2022, "Urbano Latino", "11.jpeg"),
+    new Song("Eternity", "Anyma & Chris Avantgarde", "00:05:20", "Genesys", 2023, "Techno melódico", "12.jpeg"),
+    new Song("Syren", "Anyma & Rebūke", "00:03:17", "Genesys", 2023, "Techno melódico", "13.jpeg"),
+    new Song("Simulation", "Anyma & Chris Avantgarde", "00:03:15", "Genesys", 2023, "Techno melódico", "14.jpeg"),
+    new Song("Unearth", "Anyma", "00:04:15", "Genesys", 2023, "Techno melódico", "15.jpeg"),
+    new Song("Aria", "Argy & Omnya", "00:05:15", "Genesys", 2023, "Techno melódico", "16.jpeg"),
+    new Song("Real Love", "Martin Garrix & Lloyiso", "00:03:11",  "Real Love", 2023, "EDM", "17.jpeg")
 ];
 
 // Resto del código
