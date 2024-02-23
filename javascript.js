6e4acb80403f2988f8436f236ed324441107b3c5
Sofia Macias <sofymaciasp@gmail.com>
2024-02-22 22:21:00 -0500

    Revert "sin cambios"
    This reverts commit bbc917e9299b41d2b33109c89e1e8bcfe87f17c6.

--- javascript.js
+++ javascript.js
@@ -142,7 +142,6 @@ class Player {
     stackOfSongs = [];
     audio;
     muteController;
-    currentIndex;
     lastActive; 
     
 
@@ -261,7 +260,11 @@ class Player {
 
             let iconsDiv = document.createElement("div");
             iconsDiv.className = "li_SearchResult_Group";
+<<<<<<< HEAD
             iconsDiv.innerHTML = '<button class="icon-favs-s" data-idSong = "' + stackOfSong.idSong + '"><i class="fa-regular fa-heart"></i></button><button class="icon-addPlaylist-s" data-idSong = "' + stackOfSong.idSong + '"><i class="fa-regular fa fa-plus"></i></button><button class="icon-playSong-s" data-idSong = "' + stackOfSong.idSong + '"><i class="fa-solid fa-play"></i></button>';
+=======
+            iconsDiv.innerHTML = '<button class="icon-favs" data-idSong = "' + stackOfSong.idSong + '"><i class="fa-regular fa-heart"></i></button><button class="icon-addPlaylist" data-idSong = "' + stackOfSong.idSong + '"><i class="fa-regular fa fa-plus"></i></button><button class="icon-playSong" data-idSong = "' + stackOfSong.idSong + '"><i class="fa-solid fa-play"></i></button>';
+>>>>>>> parent of bbc917e (sin cambios)
 
             listItem.appendChild(iconsDiv);
             listContainer.appendChild(listItem);
